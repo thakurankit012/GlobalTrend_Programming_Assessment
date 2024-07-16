00fb8ea0d7c2a2682451956e343c7710e392f9c9
Ankit Thakur <157781047+thakurankit012@users.noreply.github.com>
2024-07-16 17:02:47 +0530

    Create Fibonacci Sequence.js

---
+++ Fibonacci Sequence.js
@@ -0,0 +1,7 @@
+function fibonacci(n) {
+    let sequence = [0, 1];
+    for (let i = 2; i < n; i++) {
+        sequence[i] = sequence[i - 1] + sequence[i - 2];
+    }
+    return sequence.slice(0, n);
+}
