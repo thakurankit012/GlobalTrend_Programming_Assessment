492d12d955b538721affc9451f3b2b3980eaed35
Ankit Thakur <157781047+thakurankit012@users.noreply.github.com>
2024-07-16 17:03:23 +0530

    Create HashMap Implementation.js

---
+++ HashMap Implementation.js
@@ -0,0 +1,17 @@
+class HashMap {
+    constructor() {
+        this.map = {};
+    }
+
+    put(key, value) {
+        this.map[key] = value;
+    }
+
+    get(key) {
+        return this.map[key];
+    }
+
+    remove(key) {
+        delete this.map[key];
+    }
+}
